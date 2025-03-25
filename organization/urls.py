d06912034f1d6768af1e7df5d681c8e53b32ab52
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-03-25 18:40:37 +0330

    reformat

--- organization/urls.py
+++ organization/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'organization'
+app_name = "organization"
 
 urlpatterns = [
-    path('', views.OrganizationListView.as_view(), name='organization-list'),
-    
-]    path("{org_id}/", views.get_org, name="organization_entity"),
+    path("", views.OrganizationListView.as_view(), name="organization-list"),
+    path("{org_id}/", views.get_org, name="organization_entity"),
+]
