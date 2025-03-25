558e6ecc11ed6ac69630ce16078205d56c7d7edb
misano <mr.sobhan.n@outlook.com>
2025-03-25 17:25:33 +0330

    create simple org model; url file

--- organization/models.py
+++ organization/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
-# Create your models here.
 
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
