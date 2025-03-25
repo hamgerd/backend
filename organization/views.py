b2149e97eb41fe981f2802365f89c9f26e17a1db
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-03-25 18:37:47 +0330

    remove authentication classes in `OrganizationListView`

--- organization/views.py
+++ organization/views.py
@@ -1,22 +1,23 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import authentication, permissions
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Organization
+
 
 @api_view()
 def get_org(request, org_id):
     return Response({"message": "Hello, world!"})
 
+
 class OrganizationListView(APIView):
     model = Organization
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminUser]
 
     def get(self, request, format=None):
         """
         Return a list of all users.
         """
-        orgs_list =  self.model.get_all_organizations()
+        orgs_list = self.model.get_all_organizations()
         return Response(orgs_list)
