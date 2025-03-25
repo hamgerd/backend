181e7d18c09cd69a1931b9a08bf54cdfab7a2480
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-03-25 18:40:24 +0330

    update organization url pattern

--- organization/urls.py
+++ organization/urls.py
@@ -5,6 +5,5 @@ app_name = 'organization'
 
 urlpatterns = [
     path('', views.OrganizationListView.as_view(), name='organization-list'),
-    path('/{org_id}',views.get_org, name="organization_entity")
     
-]
+]    path("{org_id}/", views.get_org, name="organization_entity"),
