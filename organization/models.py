76ca9eff7562ba5c5936b904990f2382339a0137
misano <mr.sobhan.n@outlook.com>
2025-03-25 17:31:41 +0330

    add owner to org model

--- organization/models.py
+++ organization/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Organization(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     email = models.EmailField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizations')
     address = models.TextField(blank=True, null=True)
     website = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
