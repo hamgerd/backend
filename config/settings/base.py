87aad29b970c62251cbfa643bbf07ffd76ba7964
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-03-26 12:58:04 +0330

    add swagger settings for bearer auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -97,3 +97,14 @@ SIMPLE_JWT = {
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
 }
+
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+        },
+    },
+    "LOGIN_URL": "token_obtain_pair",
+}
