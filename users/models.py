cedbaa58080aaa096b8f7d2767d9b0c8d19ad905
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-03-27 01:35:01 +0330

    make email unique

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(gettext("email address"), blank=True, unique=True)
