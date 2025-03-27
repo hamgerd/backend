53bf8d1766a040fbcaacc4a1a3276369964f2859
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-03-27 22:25:22 +0330

    add email and celery settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,3 +111,17 @@ SWAGGER_SETTINGS = {
     },
     "LOGIN_URL": "token_obtain_pair",
 }
+
+# Email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+
+# Celery
+CELERY_BROKER_URL = config("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = config("CELERY_RESULT_BACKEND", None)
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
