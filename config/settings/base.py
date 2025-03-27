c565924c42f24d5838c844a0c3930e476d3723c3
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-03-27 05:21:42 +0330

    add email settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -109,3 +109,13 @@ SWAGGER_SETTINGS = {
     },
     "LOGIN_URL": "token_obtain_pair",
 }
+
+# Email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+
