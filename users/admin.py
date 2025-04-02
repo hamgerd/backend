999f7f75ef712c2d3ddd449b4c6e37bea1e31969
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-04-02 05:08:21 +0330

    add phone number to user model

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,53 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import User
 
 
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = "__all__"
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("email", "phone_number")
+
+
 @admin.register(User)
 class UserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
     list_display = ["username", "email", "is_active"]
     search_fields = ["username", "email"]
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "phone_number")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "phone_number", "password1", "password2"),
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext
 from rest_framework.exceptions import ValidationError
@@ -6,6 +7,7 @@ from rest_framework.exceptions import ValidationError
 
 class User(AbstractUser):
     email = models.EmailField(gettext("email address"), unique=True)
+    phone_number = models.CharField(max_length=11, blank=True, null=True, validators=[RegexValidator(r"^09\d{9}$")])
 
     @classmethod
     def get_by_email(cls, email: str):
