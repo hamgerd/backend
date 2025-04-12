25b3507d5524057b801676e55a31c71df9aa5878
misano <mr.sobhan.n@outlook.com>
2025-04-12 21:32:07 +0330

    fix simplejwt openapi issue

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import EmailVerifyView, UserRegisterView
 from users.views.users import PasswordResetRequestView, PasswordResetView
+from users.views.simplejwt import DecoratedTokenObtainPairView
 
 app_name = "users"
 urlpatterns = [
-    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/", DecoratedTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", UserRegisterView.as_view(), name="register"),
     path("password-reset-request/", PasswordResetRequestView.as_view(), name="password_reset_request"),
