616018d31b3e5918adde790791cb93b03869e944
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-04-16 00:47:34 +0330

    add user profile

--- users/models.py
+++ users/models.py
@@ -1,13 +1,29 @@
+import hashlib
+from io import BytesIO
+
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext
+from identiconify import PilIdenticon
 from rest_framework.exceptions import ValidationError
 
 
 class User(AbstractUser):
     email = models.EmailField(gettext("email address"), unique=True)
     phone_number = models.CharField(max_length=11, blank=True, null=True, validators=[RegexValidator(r"^09\d{9}$")])
+    profile_picture = models.ImageField(upload_to="profile_pictures/", blank=True)
+
+    def save(self, *args, **kwargs):
+        self._add_profile_picture()
+        super().save(*args, **kwargs)
+
+    def _add_profile_picture(self):
+        identicon = PilIdenticon().generate(self.username)
+        buffer = BytesIO()
+        identicon.save(buffer, format="PNG")
+        buffer.seek(0)
+        self.profile_picture.save(f"{hashlib.md5(self.username.encode()).hexdigest()}.png", buffer, save=False)
 
     @classmethod
     def get_by_email(cls, email: str):
