c72f783bc3a3e95abd285c8509a0ae9b5e324349
misano <mr.sobhan.n@outlook.com>
2025-05-31 21:16:51 +0330

    fix users ticket url issue

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,4 +1,5 @@
 from rest_framework_nested import routers
+from django.urls import path
 
 from . import views
 
@@ -10,10 +11,11 @@ router.register("", views.EventViewSet, basename="event")
 events_router = routers.NestedSimpleRouter(router, "", lookup="event")
 
 events_router.register("tickets", views.TicketViewSet, basename="ticket")
-events_router.register("tickets/me", views.UserTicketsView, basename="ticket")
 events_router.register("speakers", views.SpeakerViewSet, basename="speaker")
 
 urlpatterns = [
     *router.urls,
     *events_router.urls,
+
+    path("tickets/me/", views.UserTicketsView.as_view(), name="user-tickets"),
 ]
