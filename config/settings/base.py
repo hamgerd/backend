5ebd79e98641cf2392953ac1d1c1c9929965305b
Mahbod <74229780+M4hbod@users.noreply.github.com>
2025-07-01 18:57:37 +0330

    feat: change default pagination

--- config/settings/base.py
+++ config/settings/base.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 25,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
