62a86f7d098eba5bfc1aeb0a1b35a3e99db66621
Mahbod <74229780+M4hbod@users.noreply.github.com>
2025-07-01 19:01:52 +0330

    feat(events): register Speaker in admin

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,9 +1,10 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import Event, EventCategory, Ticket, TicketType
+from .models import Event, EventCategory, Speaker, Ticket, TicketType
 
 admin.site.register(Event)
 admin.site.register(EventCategory)
+admin.site.register(Speaker)
 admin.site.register(Ticket)
 admin.site.register(TicketType)
