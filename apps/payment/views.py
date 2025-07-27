6faf599616a0944c138c83a1918a6275efbfdd55
Mahbod <74229780+M4hbod@users.noreply.github.com>
2025-07-27 18:20:55 +0330

    fix(payment): use drf get_object_or_404

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,8 +1,7 @@
 from decouple import config
 from django.conf import settings
-from django.shortcuts import get_object_or_404
 from rest_framework import permissions
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
