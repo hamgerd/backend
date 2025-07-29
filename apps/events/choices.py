8e7920d4faedc6ce480028ad6b95802b6edf26c3
sobhan <me@mirsobhan.ir>
2025-07-30 03:01:48 +0330

    add event status

--- apps/events/choices.py
+++ apps/events/choices.py
@@ -1,6 +1,13 @@
 from django.db.models.enums import TextChoices
 
 
+class EventStatusChoice(TextChoices):
+    DRAFT       = "drf", "Draft"
+    PAUSED      = "psd", "Paused"
+    CANCELED    = "cnl", "Canceled"
+    SCHEDULED   = "sch", "Scheduled"
+    COMPLETED   = "cpl", "Completed"
+
 class TicketStatusChoice(TextChoices):
     PENDING = "p", "pending"
     SUCCESS = "s", "success"
