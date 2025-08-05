9a9a5b7f44575f25811ab45317816f7dfc5e228d
Mahbod <74229780+M4hbod@users.noreply.github.com>
2025-08-05 19:34:07 +0330

    feat(events): add title filter

--- apps/events/filters.py
+++ apps/events/filters.py
@@ -1,5 +1,11 @@
 from django_filters import rest_framework as filters
 
+from .models import Event
+
 
 class EventFilter(filters.FilterSet):
     organization_username = filters.CharFilter(field_name="organization__username", lookup_expr="iexact")
+
+    class Meta:
+        model = Event
+        fields = ["title"]
