8fdda73eaa830a55be968a79d3e1678c44db254c
Mahbod <74229780+M4hbod@users.noreply.github.com>
2025-08-05 18:17:50 +0330

    chore: update pagination page size

--- config/settings/base.py
+++ config/settings/base.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 25,
+    "PAGE_SIZE": 24,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
