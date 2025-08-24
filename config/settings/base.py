b09649cbff2100d5ca978b8bf8b905e19015fd91
Mahbod <74229780+M4hbod@users.noreply.github.com>
2025-08-24 07:01:36 +0330

    fix: use implicit ssl to send email

--- config/settings/base.py
+++ config/settings/base.py
@@ -137,7 +137,7 @@ SWAGGER_SETTINGS = {
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT", cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
